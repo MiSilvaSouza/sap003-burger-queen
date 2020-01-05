e1f05fd20d928ac678b440ec5c58b6911629a6c3
MiSilvaSouza <mirian.s2@hotmail.com>
2020-01-05 13:11:32 -0300

    pedidos enviados para cozinha

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Header from './components/header';
 import Menu from './pages/saloon';
 import Kitchen from './pages/kitchen';
+import Pending from './pages/pending';
+import Ready from './pages/ready';
 import Nav from './components/nav';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './style.css';
-
 
 
 export default function App() {
@@ -17,7 +18,9 @@ export default function App() {
         <div>              
           <Switch>
             <Route path='/saloon' component={Menu} />
-            <Route path='/kitchen' component={Kitchen} />                  
+            <Route path='/kitchen' component={Kitchen} />
+            <Route path='/pending' component={Pending} /> 
+            <Route path='/ready' component={Ready} />                   
           </Switch>  
         </div>
       </Router>
